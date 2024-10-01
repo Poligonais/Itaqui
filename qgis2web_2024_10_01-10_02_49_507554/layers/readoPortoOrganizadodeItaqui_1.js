4953311971f113fbfdcd02db0762f7e44ea4a3dd
Poligonais <cgpg@mpor.gov.br>
2024-10-01 10:17:36 -0300

    Delete qgis2web_2024_10_01-10_02_49_507554 directory

--- qgis2web_2024_10_01-10_02_49_507554/layers/readoPortoOrganizadodeItaqui_1.js
+++
@@ -1,8 +0,0 @@
-var json_readoPortoOrganizadodeItaqui_1 = {
-"type": "FeatureCollection",
-"name": "readoPortoOrganizadodeItaqui_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Name": "Área do porto organizado de Itaqui", "description": null, "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": 1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null, "Área": 5205047.4201600002 }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -44.3686111111111, -2.5828888888889 ], [ -44.369666666666703, -2.5765833333333 ], [ -44.379638888888898, -2.57 ], [ -44.37475, -2.5678333333333 ], [ -44.374111111111098, -2.5665833333333 ], [ -44.363055555555597, -2.5616111111111 ], [ -44.357361111111103, -2.5694722222222 ], [ -44.364083333333298, -2.5767777777778 ], [ -44.357111111111102, -2.6183888888889 ], [ -44.3625277777778, -2.6193055555556 ], [ -44.3686111111111, -2.5828888888889 ] ] ] } }
-]
-}
